ef7b904f1a9add72af65abd29d3200779382f7a7
Rogue Panther <roguepanthernoob@gmail.com>
2023-07-26 19:18:23 +0100

    adding tailwindcss for styling

--- nprkov/src/App.js
+++ nprkov/src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import './App.css';
+import './styles.css';
 
 
 function App() {
